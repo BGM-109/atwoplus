db8cc8ca1ebe2e2d094899ad9be5b3181749bd5c
BGM-109 <vividxxxxx@gmail.com>
2022-05-29 11:13:47 +0900

    4th commit

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const menus = ["상품", "주문", "문의"];
+  return (
+    <div>
+      <div className="flex justify-between items-center py-4 px-6">
+        <h1>에이투플샵</h1>
+        <ul className="flex space-x-4">
+          {menus.map((m, i) => (
+            <a href="/order" key={i}>
+              <li>{m}</li>
+            </a>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
